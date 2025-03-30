8363cce4a12fba866dbb5cf5142557fbfb18df07
Víctor Narváez <vicboss402018@gmail.com>
2025-03-30 15:35:06 -0500

    Estructura base para el proyecto

---
+++ src/test/java/com/jade/detect/DetectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jade.detect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
