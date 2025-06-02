7dc5d7ee44f11c13e6be043cb5a65716979e24af
Victor Narvaez <vicboss042018@gmail.com>
2025-06-02 17:53:09 -0500

    Actualiza la configuración de CORS para permitir el método PATCH en las solicitudes

--- src/main/java/com/jade/detect/config/SecurityConfig.java
+++ src/main/java/com/jade/detect/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
